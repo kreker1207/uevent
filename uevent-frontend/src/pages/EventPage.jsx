ccca2fcae4eb1806ac5c785c4b26fbb91d9c8b60
dedvnutrikirilla <93326037+dedvnutrikirilla@users.noreply.github.com>
2023-04-10 01:11:58 +0300

    Update EventPage.jsx

--- uevent-frontend/src/pages/EventPage.jsx
+++ uevent-frontend/src/pages/EventPage.jsx
@@ -205,11 +205,20 @@ export default function EventPage() {
   
 
   const buyClick = (event)=> {
-    api.post('/buy', {event_id: event.id}).
-    then((response) => {
-      console.log(response.data)
-      setBuyData({data: response.data.data, signature:  response.data.signature, isLoading: false})
-    })
+    try {
+      event.preventDefault()
+      api.post('/buy', {event_id: event.id}).then((response)=> {
+        console.log(response.data)
+        setBuyData(response.data)
+        setTimeout(()=> {
+          const form = document.getElementById('liqPayId')
+          form.submit();
+        }, 100);
+      });
+    } catch (error) {
+      console.log(error)
+    }
+    
   }
 
   return (
@@ -243,7 +252,7 @@ export default function EventPage() {
             <button
               onClick={buyClick}
             >Buy</button>
-            <form method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
+            <form id="liqPayId" method="POST" action="https://www.liqpay.ua/api/3/checkout" accept-charset="utf-8">
               <input type="hidden" name="data" value={buyData.data}/>
               <input type="hidden" name="signature" value={buyData.signature}/>
               <input type="image" src="//static.liqpay.ua/buttons/p1en.radius.png" 
